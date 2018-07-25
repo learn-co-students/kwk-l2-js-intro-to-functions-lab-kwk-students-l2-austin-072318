f80ac023f483da67203e38fec591e40be3fd2b22
KatherineMurr <katherinemurr@yahoo.com>
2018-07-25 18:49:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-// Write your code in this file!
+function getUKTime() {
+let answer = new Date() .getHours() + 5
+return answer 
+}
+
+function discountTicket(total) {
+  let discountedTicket = .2 * total
+  return total - discountedTicket
+}
+
+function tweetReply(name) {
+  return "Hey " + name + ", thanks so much for the support! I really appreciate it!"
+}
 
 
