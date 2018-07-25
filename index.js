2881b89f2ec1ab7655396e62e2f2a6670528c88e
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2018-07-25 18:46:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // Write your code in this file!
 
+function getUKTime() {
+  let newTime= new Date().getHours() + 5
+  return newTime
+}
 
+function discountTicket(ticketCost) {
+    let result = ticketCost-(ticketCost*.20)
+    return result
+}
+
+function tweetReply(fanName) {
+  let result="Hey " + fanName +", thanks so much for the support! I really appreciate it!"
+  return result
+}
