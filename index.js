24770d012f8a692b09878496c1f4ec1383f44cd0
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-25 18:29:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // Write your code in this file!
+function getUKTime(){
+ var uktime= new Date().getHours() + 5
+ return uktime
+}
 
+function discountTicket(originalPrice){
+  let discountPrice = 0.8*(originalPrice)
+  return discountPrice
+}
+
+function tweetReply(fanName){
+  return "Hey " + fanName + ", thanks so much for the support! I really appreciate it!"
+}
 
